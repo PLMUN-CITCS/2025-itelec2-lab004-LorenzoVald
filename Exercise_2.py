6c8e0219da79fa2eb618bca52479030c7449a960
LorenzoVald <24covid@gmail.com>
2025-02-13 22:49:21 +0800

    Create Exercise_2.py

---
+++ Exercise_2.py
@@ -0,0 +1,11 @@
+user  integer  =  int(input  ("Enter  an  integer:  "))  2  
+user  decimal  =  float  (input  ("Enter  a  decimal  number:  ")  3  
+u  5  e  r  t  e  x  t  =  i  n  p  u  t  (  "  E  n  t  e  r  a  s  t  r  i  n  g  :  “  )  4  
+print ("Formatted  Output  using  old-style  formatting:")  
+print ("Integer:  %d"  %  user_  integer)  5  
+print ("Decimal:  %.2f"  %  user_decimal)  6  
+print (  “  S  t  r  i  n  g  :  s  "  %  u  s  e  r  _  t  e  x  t  )  
+print("  Formatted  Output  using  f-strings:")  8  
+print(F"Integer  :  {user  integer}")  9  10  
+print  (f"  Decimal:  {user  decimal:.2f}")  #  Format  decimal  to  two  places  p
+rint  (f"String:  {user_text}")  11  ments  0  
